1c90f52a631a40f10de11a570dc5c7e3abe74439
niemubarok <niemubarok@gmail.com>
2020-12-21 19:46:03 +0700

    adding qrcode feature

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CheckInController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return "not allowed";
 });
+
+Route::get('/checkin', [CheckInController::class, 'index'])->name('form');
+Route::post('/checkin', [CheckInController::class, 'store'])->name('checkin');
+Route::post('/print', [CheckInController::class, 'print'])->name('print');
+
